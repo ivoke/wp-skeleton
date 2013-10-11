906714484c2504d22ef9d513ef3651505fab48e0
Michael Dietz <michael.dietz@ivoke.de>
2013-10-11 14:26:00 +0200

    Added loader for mu-plugins

---
+++ content/mu-plugins/load.php
@@ -0,0 +1,8 @@
+<?php
+# http://codex.wordpress.org/Must_Use_Plugins
+# WordPress only looks for PHP files right inside the mu-plugins directory, 
+# and (unlike for normal plugins) not for files in subdirectories.
+# Thus we need to load these manually
+
+# Uncomment to use root relative urls
+#require WPMU_PLUGIN_DIR.'/root-relative-urls/sb_root_relative_urls.php';
